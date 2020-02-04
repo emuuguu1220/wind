76dd107db1b563c0c2e16cbc64e67182d0474130
Munkh Erdene <munkherdene.to@gmail.com>
2020-02-04 14:42:15 -0600

    Commit

--- src/main/java/edu/miu/cs/cs544/a202001/wind/domain/Location.java
+++ src/main/java/edu/miu/cs/cs544/a202001/wind/domain/Location.java
@@ -1,83 +1,39 @@
 package edu.miu.cs.cs544.a202001.wind.domain;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-@Table(name = "locations")
+@Table(name="LOCATION")
 public class Location {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+	@Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	public Location() {}
+	
+	public Location(String name) {
+		this.name = name;
+	}
 
-    private String locationId;
-    private String building;
-    private int capacity;
-    private String name;
-    private String room;
-    private String description;
+	public Long getId() {
+		return id;
+	}
 
-    public Location(String locationId, String building, int capacity, String name, String room, String description) {
-        this.locationId = locationId;
-        this.building = building;
-        this.capacity = capacity;
-        this.name = name;
-        this.room = room;
-        this.description = description;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getLocationId() {
-        return locationId;
-    }
-
-    public void setLocationId(String locationId) {
-        this.locationId = locationId;
-    }
-
-    public String getBuilding() {
-        return building;
-    }
-
-    public void setBuilding(String building) {
-        this.building = building;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRoom() {
-        return room;
-    }
-
-    public void setRoom(String room) {
-        this.room = room;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
